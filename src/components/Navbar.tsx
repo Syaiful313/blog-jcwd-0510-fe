828587a65f5bea3d6ce3ea932c425920d7fec2eb
Syaiful313 <mthitz313@gmail.com>
2024-12-17 12:00:28 +0700

    feat: keep login

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,20 @@
+"use client";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { logoutAction } from "@/redux/slices/userSlice";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+  const user = useAppSelector((state) => state.user);
+
+  const logout = () => {
+    localStorage.removeItem("blog-storage");
+    dispatch(logoutAction());
+    router.push("/login");
+  };
+
   return (
     <nav className="border-b bg-gray-200">
       <div className="container mx-auto px-4">
@@ -9,9 +23,12 @@ const Navbar = () => {
             Logo
           </Link>
 
-          <div className="flex items-center gap-8 font-medium">
+          <div className="flex cursor-pointer items-center gap-8 font-medium">
             <Link href="/">Home</Link>
             <Link href="/">Profile</Link>
+
+            {!user.id && <Link href="/login">Sign In</Link>}
+            {!!user.id && <p onClick={logout}>Logout</p>}
           </div>
         </div>
       </div>
