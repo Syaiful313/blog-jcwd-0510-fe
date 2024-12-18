357406866ad1e35ec4fb314d53698909da565f04
syaiful <143513644+Syaiful313@users.noreply.github.com>
2024-12-18 17:55:08 +0700

    feat: blog list (#7)

---
+++ src/features/home/components/Jumbotron.tsx
@@ -0,0 +1,10 @@
+const Jumbotron = () => {
+  return (
+    <section className="flex flex-col items-center justify-center gap-3 mt-20">
+      <h1 className="text-6xl font-bold">The BlogHub</h1>
+      <p className="text-xl">A blog about food, experience, and recipes</p>
+    </section>
+  );
+};
+
+export default Jumbotron;
